874e7a485c475491f931ddad78de1d8f8d0965bb
Eugene <eugenefung79@gmail.com>
2023-06-04 14:20:22 -0400

    stub up home view and about view

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+# Create your views here.
+class Home(LoginView):
+    template_name = 'home.html'
+
+def about(request):
+  return render(request, 'about.html')
